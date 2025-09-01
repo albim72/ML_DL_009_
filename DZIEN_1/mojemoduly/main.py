18f28d9f55c63919643d8a098d1ae41688429ba6
albim72 <marcin.albiniak72@gmail.com>
2025-09-01 11:27:27 +0200

    Update main.py

--- DZIEN_1/mojemoduly/main.py
+++ DZIEN_1/mojemoduly/main.py
@@ -1,9 +1,14 @@
 # import dane
 # import dane as dn
 from dane import nrfilii as nf,book as bk
+from moje_funkcje.collfunction import czytaj_slownik,czytaj_liste
 
 #CTRL+D --> powielenie linii/bloku tekstu
 #CTRL + / --> komentowanie/odkomwntowanie bloku tekstu
 print("________________ bezpośrenio z dane ________________")
 print(nf)
 print(bk)
+
+print("________________ użycie funkcji ________________")
+czytaj_liste(nf)
+czytaj_slownik(bk)
