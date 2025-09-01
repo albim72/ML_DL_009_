bec241499f88fd98984b36fd976d7cf0a32d1c8b
albim72 <marcin.albiniak72@gmail.com>
2025-09-01 11:11:35 +0200

    Create main.py

---
+++ DZIEN_1/mojemoduly/main.py
@@ -0,0 +1,9 @@
+# import dane
+# import dane as dn
+from dane import nrfilii as nf,book as bk
+
+#CTRL+D --> powielenie linii/bloku tekstu
+#CTRL + / --> komentowanie/odkomwntowanie bloku tekstu
+print("________________ bezpośrenio z dane ________________")
+print(nf)
+print(bk)
