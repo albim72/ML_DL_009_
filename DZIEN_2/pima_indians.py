3e9fb393a15bfced663841f7ec272ac7231b66e9
albim72 <marcin.albiniak72@gmail.com>
2025-09-02 13:34:05 +0200

    Update pima_indians.py

--- DZIEN_2/pima_indians.py
+++ DZIEN_2/pima_indians.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier,plot_tree
 from sklearn import metrics
+import matplotlib.pyplot as plt
 
 col_names = ['pregnant','glucose','bp','skin','insulin','bmi','pedigree','age','label']
 pima = pd.read_csv('diabetes_pima.csv',names=col_names,header=None)
@@ -11,9 +12,9 @@ print(pima.head())
 feature_cols = ['pregnant','insulin','bmi','age','glucose','bp','pedigree']
 X = pima[feature_cols]
 y = pima.label
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-clf = DecisionTreeClassifier(max_depth=4,splitter='best',criterion='gini')
+clf = DecisionTreeClassifier(max_depth=6,splitter='best',criterion='entropy')
 clf = clf.fit(X_train,y_train)
 
 y_pred = clf.predict(X_test)
@@ -21,3 +22,13 @@ y_pred = clf.predict(X_test)
 print("Accuracy:",metrics.accuracy_score(y_test,y_pred))
 print(metrics.classification_report(y_test,y_pred))
 print(metrics.confusion_matrix(y_test,y_pred))
+
+#wykres drzewa decyzyjnego
+plt.figure(figsize=(16,8))
+plot_tree(clf,
+          feature_names=feature_cols,
+          class_names = ["No diabets","Diabetes"],
+          filled=True,rounded = True)
+plt.title("Decision Tree")
+plt.savefig("drzewo.png")
+plt.show()
